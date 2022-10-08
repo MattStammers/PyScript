9661eed8d021250899351e80ad4346f150ac3012
Block Doc <matthew.stammers@uhs.nhs.uk>
2022-10-08 14:44:34 +0100

    simple clock app partial

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PyScript
 Matt Stammers' Experiments with PyScript
+
+These examples all come from the basic pyscript tutorial elements on the official pyscript github page: https://github.com/pyscript/pyscript/blob/main/examples/simple_clock.html
---
+++ simple_clock.py
@@ -0,0 +1,41 @@
+import asyncio
+import datetime
+import time
+
+async def foo():
+  while True:
+    await asyncio.sleep(1)
+    output = datetime.datetime.now()
+    Element("outputDiv2").write(output)
+
+    out3 = Element("outputDiv3")
+    if output[-1] in ["0", "4", "8"]:
+      out3.write("It's espresso time!")
+    else:
+      out3.clear()
+
+current_time=datetime.datetime.now()
+# Printing attributes of now().
+print("The attributes of now() are :")
+ 
+print("Year :", current_time.year)
+ 
+print("Month : ", current_time.month)
+ 
+print("Day : ", current_time.day)
+ 
+print("Hour : ", current_time.hour)
+ 
+print("Minute : ", current_time.minute)
+ 
+print("Second :", current_time.second)
+ 
+print("Microsecond :", current_time.microsecond)
+
+def clock():
+    while True:
+        print(datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S"), end="\r")
+        output = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        time.sleep(1)
+
+clock()
